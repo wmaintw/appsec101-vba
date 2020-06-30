d76b6cf5044e611b6427fbf7c8fb993eca825d9f
Wei MA <wei.ma@example.com>
2020-06-30 14:41:26 +0800

    this is a vulnerable java back-end app for AppSec101 training

---
+++ src/main/java/tw/appsecvba/AppsecVbaApplication.java
@@ -0,0 +1,13 @@
+package tw.appsecvba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsecVbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsecVbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/appsecvba/AppsecVbaApplicationTests.java
@@ -0,0 +1,9 @@
+package tw.appsecvba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppsecVbaApplicationTests {
+
+}
